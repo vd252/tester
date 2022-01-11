50c979fe785470d9f0de1665f25cddcafb5efce6
vd252 <zkhalk@yandex.ru>
2022-01-11 18:35:35 +0300

    new changes

--- index.php
+++ index.php
@@ -5,6 +5,25 @@
       public $age;
       public $salary;
 
+      public function setName($name)
+      {
+          $this->name = $name;
+      }
+
+      public function setAge($age)
+      {
+          $num = $age;
+          if ($num > 18)
+          {
+              $this->age = $age;
+          }
+      }
+
+      public function setSalary($salary)
+      {
+          $this->salary = $salary;
+      }
+
       public function getName()
       {
           return $this->name;
@@ -19,16 +38,34 @@
       {
           return $this->salary;
       }
+
+      public function checkAge()
+      {
+          if ($this->age > 18){
+              return true;
+          } else {
+              return false;
+          }
+      }
+
+      public function doubleSalary($salary)
+      {
+         return $this->salary*=2;
+      }
   }
     $user1 = new Employee;
     $user1->name = 'Толя';
-    $user1->age = 34;
+    $user1->age = 25;
     $user1->salary = 3700;
 
-
+    echo $user1->checkAge() . '<br>';
     echo $user1->getName() . '<br>';
     echo $user1->getAge() . '<br>';
     echo $user1->getSalary() . '<br>';
+
+    $user1->setAge(12);
+    echo $user1->getAge() . '<br>';
+    echo $user1->doubleSalary() . '<br>';
     ?>
 
 
